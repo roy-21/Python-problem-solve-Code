907682ecd6cc0ddd5f2c26794dd92805f293fa56
Sojib Roy <rcsojib.cse1@gmail.com>
2025-07-19 23:07:28 +0600

    Add files via upload

---
+++ python/Sheet #1 (Data type - Conditions)/The Brothers.py
@@ -0,0 +1,8 @@
+F1,S1=input().strip().lower()
+F2,S2=input().strip().lower()
+
+if S1 == S2:
+    print("ARE Brothers")
+
+else:
+    print("NOT")
---
+++ python/Sheet #1 (Data type - Conditions)/Two numbers.py
@@ -0,0 +1,15 @@
+A,B=map(int,input().split())
+sum=A+B
+mul=A*B
+diff=A-B
+#if B = 0 hoy taile ei code executed hobe nh 
+#bcz amr condition deya ache b=0 
+if B != 0:
+    division=A//B
+else:
+    division = undefined
+
+print(sum)
+print(division)
+print(mul)
+print(diff)
