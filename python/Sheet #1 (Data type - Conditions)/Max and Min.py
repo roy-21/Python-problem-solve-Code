d8fcd017b3420557501e8ce229348c026f225caa
Sojib Roy <rcsojib.cse1@gmail.com>
2025-07-20 22:44:07 +0600

    Add files via upload

---
+++ python/Sheet #1 (Data type - Conditions)/Max and Min.py
@@ -0,0 +1,10 @@
+
+# Read the input
+A, B, C = map(int, input().split())
+
+# Find the minimum and maximum
+min_num = min(A, B, C)
+max_num = max(A, B, C)
+
+# Print the results
+print(min_num, max_num)
