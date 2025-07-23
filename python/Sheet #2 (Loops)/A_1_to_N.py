400aad25e349088cb64fcf9986838df366517e9a
Sojib Roy <rcsojib.cse1@gmail.com>
2025-07-23 10:01:43 +0600

    Add files via upload

---
+++ python/Sheet #2 (Loops)/A_1_to_N.py
@@ -0,0 +1,20 @@
+# contest_link__ https://codeforces.com/group/MWSDmqGsZm/contest/219158
+
+
+
+N = int(input())
+
+#using for loop
+
+for i in range(1,N+1):
+    print(i)
+
+
+
+#using while loop
+
+# N = int(input())
+# i = 1
+# while i <= N:
+#     print(i)
+#     i += 1
