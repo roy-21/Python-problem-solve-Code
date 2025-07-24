068eda1b936223bfd688218906881ae96f5803c4
Sojib Roy <rcsojib.cse1@gmail.com>
2025-07-24 14:09:07 +0600

    Add files via upload

---
+++ python/Sheet #2 (Loops)/D_Fixed_Password.py
@@ -0,0 +1,11 @@
+# contest_link__ https://codeforces.com/group/MWSDmqGsZm/contest/219158
+
+
+while True:
+    X = int(input())
+
+    if X == 1999:
+        print("Correct")
+        break
+    else:
+        print("wrong")
