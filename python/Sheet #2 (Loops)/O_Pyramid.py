f8988a850d998fd8779af5349691ab9e26fb8c0e
Sojib Roy <rcsojib.cse1@gmail.com>
2025-07-30 01:54:13 +0600

    Add files via upload

---
+++ python/Sheet #2 (Loops)/O_Pyramid.py
@@ -0,0 +1,8 @@
+# contest_link__ https://codeforces.com/group/MWSDmqGsZm/contest/219158
+
+
+N = int(input().strip())  #Convert input to integer
+
+
+for i in range(1, N + 1): #Loop from 1 to N
+    print('*' * i)  #Print i stars in each row
