fd38dd8d1584d5bdb4d211cc78d7bcbcaf6eb1d8
Sojib Roy <rcsojib.cse1@gmail.com>
2025-08-05 15:59:12 +0600

    Add files via upload

---
+++ python/Sheet #2 (Loops)/Z_Three_Numbers.py
@@ -0,0 +1,12 @@
+# contest_link__ https://codeforces.com/group/MWSDmqGsZm/contest/219158
+
+
+K, S = map(int,input().split())
+count = 0
+
+for X in range(K+1):
+    for Y in range(K+1):
+        Z = S - X - Y
+        if 0 <= Z <= K:
+            count += 1
+print(count) 
