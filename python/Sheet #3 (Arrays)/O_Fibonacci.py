81bfca55b6f72468ff66fbdf73946219fb9d3e2a
Sojib Roy <rcsojib.cse1@gmail.com>
2025-09-05 02:35:41 +0600

    Add files via upload

---
+++ python/Sheet #3 (Arrays)/O_Fibonacci.py
@@ -0,0 +1,14 @@
+#Recursive Fibonacci function
+def fib(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+#Input
+N = int(input())
+
+#Output
+print(fib(N))
